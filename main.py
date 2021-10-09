16aa0347c4e4eeb718ceeb9bb07835a60a815c58
M-Vaidehi-Ramesh <m_ramesh150220@fanshaweonline.ca>
2021-10-09 16:38:00 -0400

    addingpython

---
+++ main.py
@@ -0,0 +1,8 @@
+name ="mamatha"
+eyecolour ="black"
+heightincm ="153"
+haircolour ="black"
+
+print("my name is" +name "and i have" + eyecolour + "eyes" )
+print("i am about" + heightincm + "tall and have" + hairclour +"hair")
+print("i look a little fraggile actually")
