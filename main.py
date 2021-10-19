9ee1831cab9a003e7292ac3aeef748d263c13cac
M-Vaidehi-Ramesh <m_ramesh150220@fanshaweonline.ca>
2021-10-19 14:56:43 -0400

    python_rps_updated

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
-name ="mamatha"
-eyecolour ="black"
-heightincm ="153"
-haircolour ="black"
+from random import randint
 
-print("my name is" +name "and i have" + eyecolour + "eyes" )
-print("i am about" + heightincm + "tall and have" + hairclour +"hair")
-print("i look a little fraggile actually")
+#save the player as a variable called player
+#the value of player will be one of three choices to type (input)
+
+#add player and computer lives
+
+playerlives=5
+computerlives=5 
+
+player = input("Choose rock, paper or scissors: ");
+
+print("player chose: " + player)
+
+#an array is just a container. It holds multiple values in a 0-based index
+# you can store anything in an array and retrieve it later. Arrays have square bracket notations
+choices = ["rock", "paper", "scissors"]
+
+
+
+computer = choices[randint(0,2)]
+print("computer chose: " + computer)
+
+if(computer==player):
+	print("tie!..try again")
+
+if(player =="rock"):
+	if(computer=="paper"):
+		print("you lose!")
+		 playerlives=playerlives-1
+		
+	else:
+	    print("you win!!!")
+	    computerlives=computerlives-1
+	    
+	    
+	
+
+
+elif(player =="paper"):
+	if(computer =="scissors"):
+		print("you lose!")
+		 playerlives=playerlives-1
+		
+	else:
+	     print("you win!!!")
+	     computerlives =computerlives-1
+	      
+	
+   	
+
+ 
+elif(player =="scissors"):
+	if(computer=="rock"):
+		print("you lose!")
+		 playerlives=playerlives-1
+		
+	else:
+	    print("you win!!!")
+         computerlives=computerlives-1
+        
+
+
+
+
+
+print("playerlives:" + str(playerlives)) 
+print("playerlives:" + str(computerlives))
+       
+	
